193902d1574e7059e93c61fcbe738e6b34888631
sai <pallesp@amazon.com>
2020-10-14 23:10:58 -0700

    HTML vs JSX fixes

--- src/index.js
+++ src/index.js
@@ -6,7 +6,21 @@ import ReactDOM from 'react-dom';
 // Create a react component
 
 const App = () => {
-    return <div> Hi there </div>;
+
+    const buttonText = { text : 'Click Me!'};
+    const labelText = 'Enter Name:';
+
+    return (
+        <div>
+            <label htmlFor="name" className="label">
+                {labelText}
+            </label>
+            <input id="name" type="text" />
+            <button style={{backgroundColor:'blue', color: 'white'}}>
+                {buttonText.text}
+            </button>
+        </div>
+    );
 }
 
 // Take the react component and show on the screen
