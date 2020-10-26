3e783979710141f122930b51af792fcbf63074d6
sai <pallesp@amazon.com>
2020-10-26 12:17:24 -0700

    Duplicating the single component

--- src/index.js
+++ src/index.js
@@ -13,15 +13,6 @@ const App = () => {
 
     return (
         <div>
-            <label htmlFor="name" className="label">
-                {labelText}
-            </label>
-            <input id="name" type="text" />
-            <button style={{backgroundColor:'blue', color: 'white'}}>
-                {buttonText.text}
-            </button>
-            <br/>
-
             <div className="ui container comments">
                 <div className="comment">
                     <a href="/" className="avatar">
@@ -30,6 +21,40 @@ const App = () => {
                     <div className="content">
                         <a href="/" className="author">
                             Sam
+                        </a>
+                        <div className="metadata">
+                            <span className="date">Today at 6.00PM</span>
+                        </div>
+                        <div className="text">Nice blog post</div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="ui container comments">
+                <div className="comment">
+                    <a href="/" className="avatar">
+                        <img alt="avatar" src={faker.image.avatar()} />
+                    </a>
+                    <div className="content">
+                        <a href="/" className="author">
+                            John
+                        </a>
+                        <div className="metadata">
+                            <span className="date">Today at 6.00PM</span>
+                        </div>
+                        <div className="text">Nice blog post</div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="ui container comments">
+                <div className="comment">
+                    <a href="/" className="avatar">
+                        <img alt="avatar" src={faker.image.avatar()} />
+                    </a>
+                    <div className="content">
+                        <a href="/" className="author">
+                            Sai
                         </a>
                         <div className="metadata">
                             <span className="date">Today at 6.00PM</span>
