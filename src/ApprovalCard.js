0eaf1d7f43c90cc94d30160b3f62f81e8b2579d5
sai <pallesp@amazon.com>
2020-10-26 15:55:54 -0700

    Wrap CommentDetails Componenet in ApprovalCard component

--- src/ApprovalCard.js
+++ src/ApprovalCard.js
@@ -1,11 +1,13 @@
 import React from "react";
 
-const ApprovalCard = () => {
+const ApprovalCard = (props) => {
+
+    console.log("Props in Approval Card", props);
 
     return (
 
         <div className="ui card">
-            <div className="content"> Are you Sure ? </div>
+            <div className="content"> {props.children} </div>
             <div className="extra content">
                 <div className="ui two buttons">
                     <div className="ui basic green button"> Approve </div>
--- src/index.js
+++ src/index.js
@@ -13,26 +13,38 @@ const App = () => {
 
     return (
         <div className="ui container comments">
-            <CommentDetail
-                author="Sai Krishna"
-                timeAgo="Today at 4.15PM"
-                commentText="Nice blog post"
-                avatarSrc={faker.image.avatar()}
-            />
-            <CommentDetail
-                author="Shiva"
-                timeAgo="Today at 1.00PM"
-                commentText="Awesome post"
-                avatarSrc={faker.image.avatar()}
-            />
-            <CommentDetail
-                author="Charan"
-                timeAgo="Today at 5.55PM"
-                commentText="Really loved this post"
-                avatarSrc={faker.image.avatar()}
-            />
 
-            <ApprovalCard />
+            <ApprovalCard>
+                <div>
+                    <h4>Warning!</h4>
+                    Are you sure ?
+                </div>
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail
+                    author="Shiva"
+                    timeAgo="Today at 1.00PM"
+                    commentText="Awesome post"
+                    avatarSrc={faker.image.avatar()}
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail
+                    author="Charan"
+                    timeAgo="Today at 5.55PM"
+                    commentText="Really loved this post"
+                    avatarSrc={faker.image.avatar()}
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail
+                    author="Sai Krishna"
+                    timeAgo="Today at 4.15PM"
+                    commentText="Nice blog post"
+                    avatarSrc={faker.image.avatar()}
+                />
+            </ApprovalCard>
+
         </div>
     );
 };
